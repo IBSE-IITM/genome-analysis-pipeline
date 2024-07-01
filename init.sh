06164cd972e08710e0c222a90b979b4c8d1e4c61
ayamgupta <ayam.gupta@gmail.com>
2024-07-01 14:00:08 +0530

    Describe your changes here

---
+++ init.sh
@@ -0,0 +1,13 @@
+echo $(date): 'Initializing Please Wait -- This might take time depending on your connection speed.'
+
+mkdir data/
+
+cd data/ 
+
+./download.sh
+
+mkdir samples
+
+echo 'Copy/Move the samples in fastq.gz format to the samples directory found on the local machine'
+
+echo 'Initializing COMPLETE' $(date)
